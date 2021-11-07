a3ca2b6353fb279ab29aadcbd54984458456d1ae
Oleksandr Pavlenko <oleksandr.pavlenko.op@gmail.com>
2021-11-07 16:49:06 +0300

    add the concept of facade pattern

---
+++ src/net/pavlenko/basepatterns/structural/facade/SprintRunner.java
@@ -0,0 +1,9 @@
+package net.pavlenko.basepatterns.structural.facade;
+
+public class SprintRunner {
+    public static void main(String[] args) {
+        Workflow workflow = new Workflow();
+
+        workflow.solveProblems();
+    }
+}
